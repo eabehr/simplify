869aba9acf144b73ed6b05989e40964287c3357f
eabehr <emilyabehrendt@gmail.com>
2018-08-03 18:23:17 -0700

    revert changes to dropdowns

--- client/src/components/CreateItemRequest.js
+++ client/src/components/CreateItemRequest.js
@@ -64,8 +64,8 @@ class CreateItemRequest extends Component {
         );
         return (
             <div>
+                <label>Item category:</label><br/>
                 <select onChange={this.handleCategoryChange}>
-                    <option disabled selected>Select category:</option>
                     {dropdownOptions}
                 </select>
             </div>
@@ -83,8 +83,8 @@ class CreateItemRequest extends Component {
             );
             return (
                 <div>
+                    <label>Item Type</label><br/>
                     <select onChange={this.handleItemTypeChange}>
-                        <option disabled selected>Select item:</option>
                         {itemOptions}
                     </select>
                 </div>)
@@ -119,8 +119,8 @@ class CreateItemRequest extends Component {
                  
                 sizeDropdown = (
                     <div>
+                        <label>Size</label><br/>
                         <select>
-                            <option disabled selected>Select size:</option>
                             {sizeOptions}
                         </select>
                     </div>)
@@ -132,8 +132,8 @@ class CreateItemRequest extends Component {
 
                 genderDropdown = (
                     <div>
+                        <label>Gender</label><br/>
                         <select>
-                            <option disabled selected>Select gender:</option>
                             {genderOptions}
                         </select>
                     </div>)
@@ -144,8 +144,8 @@ class CreateItemRequest extends Component {
                 );
                 styleDropdown = (
                     <div>
+                        <label>Style</label><br/>
                         <select>
-                            <option disabled selected>Select style:</option>
                             {styleOptions}
                         </select>
                     </div>)
@@ -180,9 +180,7 @@ class CreateItemRequest extends Component {
                     <input required type="text" name="clientId" value={this.state.value} onChange={this.handleFormChange} className="form-control" />
                     </label>
                     <br /><br />
-
                     { categoryDropdown }
-                    
                     <br/><br/>
                     { itemsInCategoryDropdown }
                     <br/><br/>
@@ -201,6 +199,8 @@ class CreateItemRequest extends Component {
 export default CreateItemRequest;
 
 /**
- * TODO:
- * - some issues with the automatically selected disabled value when changing between category types
+ * TODO
+ * 
+ * Can set labal in dropdown like this, but poses some issues
+ * <option disabled selected>Select category:</option>
  */
